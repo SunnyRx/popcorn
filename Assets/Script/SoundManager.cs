5bc6e95e1348e5aa3aee40a397c47542d520dc38
SunnyRx <Sunny558@qq.com>
2017-12-28 15:05:35 +0900

    add timer, add title bgm

---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource mainAudioSource;
+
+    static SoundManager _instance;
+
+    
+
+    static public SoundManager getInstance()
+    {
+        if (!_instance)
+        {
+            _instance = new SoundManager();
+        }
+        return _instance;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playSound(AudioClip clip)
+    {
+        mainAudioSource.clip = clip;
+        mainAudioSource.Play();
+    }   
+
+}
---
+++ Assets/Script/Timer.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+
+var timer : float = 0;
+
+function Update(){
+	timer -= Time.deltaTime;
+
+	if(timer <= 0){
+		timer = 0;
+	}
+}
+
+function OnGUI(){
+	if(timer > 0){
+		GUI.Box(new Rect(715, 50, 190, 25), "Time Remaining : " + timer.ToString("0"));
+	} else {
+		gameOver();
+	}
+
+}
+
+function gameOver(){
+	GUI.Box(new Rect(715, 50, 190, 25), "Game Over");
+	SceneManager.LoadScene("ResultScene");
+}

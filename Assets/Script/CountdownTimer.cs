476a26c2a2b1523b7cf1947a12d9de9be31e1a7f
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-01-01 13:11:36 +0900

    replace JS timer with C# timer

---
+++ Assets/Script/CountdownTimer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour {
+    float timeRemaing = 60;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeRemaing -= Time.deltaTime;
+	}
+
+    private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25; //change the font size
+
+        if (timeRemaing > 0)
+        {
+            GUI.Label(new Rect(1100, 100, 200, 200), "Time Remaining : " + (int)timeRemaing, guiStyle);
+        }
+        else
+        {
+            GUI.Label(new Rect(100, 100, 200, 100), "Time's up");
+            SceneManager.LoadScene("ResultScene");
+        }
+
+    }
+}

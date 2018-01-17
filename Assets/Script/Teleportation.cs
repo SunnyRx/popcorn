3cfadd6d846d6f1904c035909c6ad82223054e6d
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-01-17 19:56:29 +0900

    add teleport functionality

---
+++ Assets/Script/Teleportation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour {
+
+    public GameObject entryPortal;
+    public Transform exitPortal;
+
+    private bool playerIsOverLapping = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverLapping = true;
+            other.gameObject.transform.position = exitPortal.position;
+        }
+    }
+}
+
+
